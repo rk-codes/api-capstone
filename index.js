bcd0213b202bd1c54ec7ec37e7d713f1df602d9e
rk-codes <ping.you.back@gmail.com>
2017-10-06 01:40:08 -0700

    retrieve categories

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
 const API_KEY = "H5GX62DOZ35YWUCY36J7";
 const EVENTBRITE_URL = "https://www.eventbriteapi.com/v3";
 
-function getDataFromApi(endpoint, callback) {
+function getCategoriesFromApi(endpoint, callback) {
   console.log("Enter")
   const settings = {
-    url: `${EVENTBRITE_URL}/${endpoint}`,
+    url: `${EVENTBRITE_URL}/${endpoint}/`,
     crossDomain: true,
     data: {
       token: API_KEY
     },
-   // dataType: 'jsonp',
+    dataType: 'json',
     type: 'GET',
     success: callback,
     failure: logData
@@ -23,5 +23,15 @@ function getDataFromApi(endpoint, callback) {
 function logData(data){
   console.log(data);
 }
-getDataFromApi("categories",logData);
+//getCategoriesFromApi("categories",logData);
 
+function getEventsByLocation(location, callback){
+  const params= {
+    "location.address": `${location}`,
+    token: API_KEY
+  }
+  $.getJSON(`${EVENTBRITE_URL}/events/search/`,params,logData);
+
+}
+let user_location="California";
+getEventsByLocation(user_location,logData);
