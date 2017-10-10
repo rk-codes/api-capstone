b5d3af544039c1475580ab2168372f87acbd7f5f
rk-codes <ping.you.back@gmail.com>
2017-10-09 23:54:32 -0700

    initial commit

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function getEventsByLocation(location, callback){
 }
 
 function renderEventInfo(result) {
+  $('.result-section').show();
   $('.location-box').show();
   $('.events-list').show();
   $('.map').show();
@@ -30,8 +31,10 @@ function renderEventInfo(result) {
   let eventLogo = result.logo.original.url;
   let fromDate = result.start.local;
   let toData = result.end.local;
-  console.log("logo:" + eventLogo);
-  console.log(`Name: ${eventName} Description: ${eventDescription} Details: ${eventDetailsUrl}`);
+  let venueId = result.venue_id;
+  
+  getEventVenueDetails(venueId,logData);
+
   $('.events-list').append(`
     <div class="js-eventInfo">
       <img class="event-image" src="${eventLogo}">
@@ -43,6 +46,7 @@ function renderEventInfo(result) {
 // Displays the details of the events retrieved
 function displayEvents(data) {
   console.log("displayEvents");
+
   const results = data.events.map((item,index) => renderEventInfo(item));
   $('.search-box').hide();
 }
@@ -55,12 +59,35 @@ function handleSearchClick() {
     console.log("Location: "+ location);
     getEventsByLocation(location,displayEvents);
    })
+ 
 }
+
+// Get the venue details of each event retrieved
+function getEventVenueDetails(id,callback){
+  // console.log(" getEventVenueDetails Venue Id: "+id);
+  const params= {
+    token: API_KEY
+  }
+   $.getJSON(`${EVENTBRITE_URL}/venues/${id}/`,params,callback);
+}
+function logData(data){
+  console.log("Success");
+}
+
+// Google Map
+function initMap(){
+  console.log("Init Map");
+  let options = {
+    zoom: 8,
+    center: {lat: 37.7749, lng: -122.4194}
+  }
+  let map = new google.maps.Map(document.getElementById('map'),options);
+}
+
 
 function init(){
   handleSearchClick();
   $('.events-list').hide();
-  $('.map').hide();
-  $('.location-box').hide();
+  $('.result-section').hide();
 }
 $(init());
