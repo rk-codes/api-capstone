7744ec6303d4a80bae368c45a8c9328de0a56656
rk-codes <ping.you.back@gmail.com>
2017-10-12 00:45:08 -0700

    add event markers on map

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const API_KEY = "H5GX62DOZ35YWUCY36J7";
 const EVENTBRITE_URL = "https://www.eventbriteapi.com/v3";
+const MAP_CENTER = "https://maps.googleapis.com/maps/api/geocode"
 let map;
 let inputLocation;
 
@@ -17,17 +18,32 @@ function getCategoriesFromApi(endpoint, callback){
 
 // Calls the api to get the events in the given location
 function getEventsByLocation(location, callback){
+  $('.search-box').hide();
   const params= {
-    "location.address": `${location}`,
-    token: API_KEY
-  }
+      "location.address": `${location}`,
+      token: API_KEY
+     }
   $.getJSON(`${EVENTBRITE_URL}/events/search/`,params,callback);
+  //displayMap(location);  
 
 }
 
+function displayMap(location){
+
+  //$('.map').show();
+  $.getJSON(`${MAP_CENTER}/json?address=${location}`,function (data){
+      console.dir(data);
+      initMap(data.results[0].geometry.location.lat, data.results[0].geometry.location.lng);
+          
+   });
+
+}
 // Success callback. Pass the data recieved to next request to collect venue details
 function getDataOfEvents(data) {
-   $('.search-box').hide();
+    $('.search-box').hide();
+    $('.result-section').show();
+    $('.events-list').show();
+    displayMap(inputLocation);
    data.events.forEach((event) =>{
     getEventVenueDetails(event);
   });
@@ -44,18 +60,18 @@ function getEventVenueDetails(eventData){
      console.log("success call back venue");
      console.log(data.address.localized_address_display);
      eventData.address = data.address.localized_address_display;
+     eventData.latitude = data.latitude;
+     eventData.longitude = data.longitude;
      console.log(eventData);
      renderEventInfo(eventData);
+          
+     addMarkerToMap(eventData.latitude, eventData.longitude);
    });
-}
+ }
 
 
 // This function renders the DOM with the event data
 function renderEventInfo(eventResult){
-  $('.result-section').show();
-  $('.events-list').show();
-  $('.map').show();
-  initMap();
   let eventName = eventResult.name.text;
   let eventDescription = eventResult.description.text;
   let eventDetailsUrl = eventResult.url;
@@ -63,6 +79,9 @@ function renderEventInfo(eventResult){
   let fromDate = eventResult.start.local;
   let toData = eventResult.end.local;
   let eventAddress = eventResult.address;
+  console.log(`Event start: ${fromDate}`);
+  console.dir(fromDate);
+  console.log(new Date(fromDate));
 
   $('.events-list').append(`
      <div class="js-eventInfo">
@@ -82,22 +101,35 @@ function handleSearchClick() {
     inputLocation = $('.user-location').val();
     $('.location-box').show();
     $('.location-box').append(`<span class="current-location">Upcoming events in ${inputLocation}`);
-    getEventsByLocation(inputLocation,getDataOfEvents);
-   })
-  
+    getEventsByLocation(inputLocation, getDataOfEvents);
+    
+  })
+}
+
+// Add marker to google map
+function addMarkerToMap(eLat, eLong) {
+  //let eventLatLng = new google.maps.LatLng(lat, long);
+  console.log({lat: eLat, lng: eLong});
+  let marker = new google.maps.Marker({
+        position: {lat: Number(eLat), lng: Number(eLong)},
+        map: map
+  });
 }
 
 // Google Map
-function initMap(){
-
+function initMap(lat, lng){
+  $('.map').show();
   console.log("Init Map");
+  console.log(lat);
+  console.log(lng);
   let options = {
-    zoom: 8,
-    // center: {lat: 37.6213, lng: -37.6213}
+    zoom: 10,
+     //center: {lat: lat, lng: lng}
   }
    map = new google.maps.Map(document.getElementById('map'),options);
-   google.maps.event.trigger(map, 'resize');
-    map.setCenter(new google.maps.LatLng(38.9072,-77.0369));
+   //google.maps.event.trigger(map, 'resize');
+   map.setCenter(new google.maps.LatLng(lat, lng));
+
 }
 
 
@@ -105,6 +137,7 @@ function init(){
   handleSearchClick();
   $('.events-list').hide();
   $('.result-section').hide();
- 
+  let input = document.getElementById('autocomplete');
+  let autocomplete = new google.maps.places.Autocomplete(input);
 }
 $(init());
