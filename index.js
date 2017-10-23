6b7c3d1d8f56e2c2e70d795fc3254cd62154256e
rk-codes <ping.you.back@gmail.com>
2017-10-22 23:16:45 -0700

    fix issue markers not getting displayed occassionally
    Had to do with the sequence in which map was created. Instead of creating the map
    in the success callback of geocoding request, it is now done just before making that request.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const API_KEY = "H5GX62DOZ35YWUCY36J7";
 const EVENTBRITE_URL = "https://www.eventbriteapi.com/v3";
+const MAP_ICON_DEFAULT = "marker_initial.png"
+const MAP_ICON_SELECTED = "marker_new.png"
 const MAP_URL = "https://maps.googleapis.com/maps/api/geocode"
 let map;
-let infowindow;
+let mapInfoWindow;
 let inputLocation;
 let allMarkers = [];
 
@@ -15,21 +17,29 @@ function getEventsByLocation(location, callback){
   $.getJSON(`${EVENTBRITE_URL}/events/search/`, params, callback);
 }
 
-// Calls google map api
-function initializeMap(location){
-  $.getJSON(`${MAP_URL}/json?address=${location}`, function (data){
-    initMap(data.results[0].geometry.location.lat, data.results[0].geometry.location.lng);        
-  });
-}
-
 // This function is the callback of retrieving the events. 
 // The data recieved is passed to next ajax request to collect venue details
-function getDataOfEvents(data) {
+function handleEventData(data) {
   $('#loading').hide();
   $('.result-section').show();
   initializeMap(inputLocation);
   data.events.forEach((event) => {
     getEventVenueDetails(event);
+  });
+}
+
+// Calls google map api and centers the map on user input location
+function initializeMap(location) {
+  let options = {
+    zoom: 10,
+  }
+  map = new google.maps.Map($('#map').get(0), options);
+  mapInfoWindow = new google.maps.InfoWindow({});
+  $.getJSON(`${MAP_URL}/json?address=${location}`, function (data) {
+    let latitude = data.results[0].geometry.location.lat;
+    let longitude = data.results[0].geometry.location.lng;
+    map.setCenter(new google.maps.LatLng(latitude, longitude));      
+    $('#map').show();
   });
 }
 
@@ -97,7 +107,7 @@ function handleSearchClick() {
     $('.location-box').append(`
       <span class="current-location">Upcoming events in <strong>${inputLocation}</strong></span>
       <input type="button" value="New Search" class="js-searchagain-button">`);
-    getEventsByLocation(inputLocation, getDataOfEvents);
+    getEventsByLocation(inputLocation, handleEventData);
     handleNewSearchClick();
   });
 }
@@ -115,7 +125,7 @@ function handleNewSearchClick() {
   });
 }
 
-// Add marker to google map and set content of the info window
+// Add marker to google map and set the content of the info window
 function addMarkerToMap(eventData) {
   let eLat = eventData.latitude;
   let eLong = eventData.longitude;
@@ -131,30 +141,30 @@ function addMarkerToMap(eventData) {
     minute: '2-digit'
   });
   let marker = new google.maps.Marker({
-    position: {lat: Number(eLat), lng: Number(eLong)},
+    position: new google.maps.LatLng(Number(eLat), Number(eLong)),
     map: map,
     name: eName
   });
-  marker.setIcon("marker_initial.png");
+  marker.setIcon(MAP_ICON_DEFAULT);
   google.maps.event.addListener(marker, 'click', function() {
-  infowindow.setContent(`
+  mapInfoWindow.setContent(`
     <div class="js-marker-window">
       <a href="${eventDetails}" target="_blank">${eName}</a>
       <p class="js-marker-date">${formatter.format(new Date(eventDate))}</p>
       <p>${venAddress}</p>
     </div>`);
-    infowindow.open(map,marker);
+    mapInfoWindow.open(map,marker);
   });
   allMarkers.push(marker); 
 }
 
 // Sets a new marker for an event when mouse moved over that event in the list
-function handleMouseOver(){
+function handleMouseOverEventInList(){
   $('.events-list').on('mouseover', '.js-eventInfo',function(event) {
     let name = event.currentTarget.getAttribute("data-name");
     for(let i = 0; i < allMarkers.length; i++) {
       if(name === allMarkers[i].name) {
-        allMarkers[i].setIcon("marker_new.png");
+        allMarkers[i].setIcon(MAP_ICON_SELECTED);
         break;
       }
     }
@@ -162,38 +172,26 @@ function handleMouseOver(){
 }
 
 // Sets the marker to the initial icon when mouse moved out of the event in the list
-function handleMouseOut(name){
+function handleMouseOutEventInList(name){
   $('.events-list').on('mouseout', '.js-eventInfo',function(event) {
     let name = event.currentTarget.getAttribute("data-name");
     for(let i = 0; i < allMarkers.length; i++) {
       if(name === allMarkers[i].name) {
-        allMarkers[i].setIcon("marker_initial.png");
+        allMarkers[i].setIcon(MAP_ICON_DEFAULT);
         break;
       }
     }
   });
 }
 
-// Initialize google map centered on user input location
-function initMap(lat, lng) {
-  $('.map').show();
-  let options = {
-    zoom: 10,
-  }
-   map = new google.maps.Map(document.getElementById('map'),options);
-   map.setCenter(new google.maps.LatLng(lat, lng));
-   infowindow = new google.maps.InfoWindow({});
-}
-
 // Initial settings
 function init() {
-  handleSearchClick(); 
-  handleMouseOver();
-  handleMouseOut();
+  handleSearchClick();
+  handleMouseOverEventInList();
+  handleMouseOutEventInList();
   $('#loading').hide();
   $('.result-section').hide();
   let input = $('#autocomplete').get(0);
   let autocomplete = new google.maps.places.Autocomplete(input);
 }
-
 $(init());
